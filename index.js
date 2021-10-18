617955ac3978d33eae293b221dc8193f66cb7dcb
Walter-Jucah <52893282+Walter-Jucah@users.noreply.github.com>
2021-10-17 23:39:58 -0300

    Update index.js
    typo error on https

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let weather = {
   apiKey: "be696f95f3cb26c716958667f9d0f1ac",
   fetchWeather: function (city) {
     fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
         "&units=metric&appid=" +
         this.apiKey,
